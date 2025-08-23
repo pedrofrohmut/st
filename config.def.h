49bebe84f9ec069c8af4e8d0ecf8bc004a4a92ad
pedrofrohmut <pedrofrohmut@gmail.com>
2025-08-23 01:48:17 -0300

    Scrollback nows scrolls 5 lines and is not super slow anymore

--- config.def.h
+++ config.def.h
@@ -186,6 +186,9 @@ static unsigned int defaultattr = 11;
  */
 static uint forcemousemod = ShiftMask;
 
+// The amount of lines to scroll (speed)
+const unsigned int mousescrollincrement = 1;
+
 /*
  * Internal mouse shortcuts.
  * Beware that overloading Button1 will disable the selection.
@@ -194,6 +197,8 @@ static MouseShortcut mshortcuts[] = {
 	/* mask                 button   function        argument       release */
 	{ ShiftMask,            Button4, kscrollup,      {.i = 1} },
 	{ ShiftMask,            Button5, kscrolldown,    {.i = 1} },
+	{ ShiftMask,            Button4, kscrollup,      {.i = mousescrollincrement} },
+	{ ShiftMask,            Button5, kscrolldown,    {.i = mousescrollincrement} },
 	{ XK_ANY_MOD,           Button2, selpaste,       {.i = 0},      1 },
 	{ ShiftMask,            Button4, ttysend,        {.s = "\033[5;2~"} },
 	{ XK_ANY_MOD,           Button4, ttysend,        {.s = "\031"} },
